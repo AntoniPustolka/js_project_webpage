61d8225cc6c1a741e494cee73fba75fb770e4427
Antoni Pustolka <antonipustolka@gmail.com>
2024-01-07 21:59:37 +0100

    Product Filter

--- main.js
+++ main.js
@@ -1,52 +1,128 @@
+const galleryImages = [
+  {
+    src: "./assets/gallery/img1.png",
+    alt: "Thumbnail Image 4",
+  },
+  {
+    src: "./assets/gallery/image1.jpg",
+    alt: "Thumbnail Image 1",
+  },
+  {
+    src: "./assets/gallery/image2.jpg",
+    alt: "Thumbnail Image 2",
+  },
+  {
+    src: "./assets/gallery/image3.jpg",
+    alt: "Thumbnail Image 3",
+  },
+];
+
+const product = [
+    {
+      title: "AstroFiction",
+      author: "John Doe",
+      price: 49.9,
+      image: "./assets/products/img6.png"
+    },
+    {
+      title: "Space Odissey",
+      author: "Marie Anne",
+      price: 35,
+      image: "./assets/products/img1.png"
+    },
+    {
+      title: "Doomed City",
+      author: "Jason Cobert",
+      price: 0,
+      image: "./assets/products/img2.png"
+    },
+    {
+      title: "Black Dog",
+      author: "John Doe",
+      price: 85.35,
+      image: "./assets/products/img3.png"
+    },
+    {
+      title: "My Little Robot",
+      author: "Pedro Paulo",
+      price: 0,
+      image: "./assets/products/img5.png"
+    },
+    {
+      title: "Garden Girl",
+      author: "Ankit Patel",
+      price: 45,
+      image: "./assets/products/img4.png"
+    }
+  ]
+
 // !! Menu Section !!
+function manuHandler() {
+  document
+    .querySelector("#open-nav-menu")
+    .addEventListener("click", function () {
+      document.querySelector("header nav .wrapper").classList.add("nav-open");
+    });
 
-document.querySelector("#open-nav-menu").addEventListener("click", function(){
-    document.querySelector("header nav .wrapper").classList.add("nav-open");
-});
-
-document.querySelector("#close-nav-menu").addEventListener("click", function(){
-    document.querySelector("header nav .wrapper").classList.remove("nav-open");
-});
-
-// !! Greeting Section !!
-
-function celsiusToFahr(temp){
-    return ((parseFloat(temp) * 1.8) + 32);
+  document
+    .querySelector("#close-nav-menu")
+    .addEventListener("click", function () {
+      document
+        .querySelector("header nav .wrapper")
+        .classList.remove("nav-open");
+    });
+}
+// Temperature conversion
+function celsiusToFahr(temp) {
+  return parseFloat(temp) * 1.8 + 32;
 }
 
+// !! Greeting Section !!
+function greetingHandler() {
+  let currentHour = 10;
+  new Date().getHours();
+  // console.log(currentHour);
+  let greetingText;
+  if (currentHour < 12) {
+    greetingText = "Good Morning";
+  } else if (currentHour < 19) {
+    greetingText = "Good Afternoon";
+  } else if (currentHour < 24) {
+    greetingText = "Good Evening";
+  } else {
+    greetingText = "Welcome!";
+  }
 
-const greetingText = "Good Day Darling";
-const weatherCondition = "sunny";
-const userLocation = "New York";
-let temperature = 22.8221;
+  // const greetingText = "Good Day Darling";
+  const weatherCondition = "sunny";
+  const userLocation = "New York";
+  let temperature = 22.8221;
 
-let weatherText;
+  let weatherText;
 
-// alert("The temperature outside is " + celsiusToFahr(temperature) + "°F outside.")
+  // alert("The temperature outside is " + celsiusToFahr(temperature) + "°F outside.")
+  // console.log(weatherText); (event.target.id) === "celsius"
+  // !! Farh to Celsius => Redio Buttons !!
 
+  document
+    .querySelector(".weather-group")
+    .addEventListener("click", function (event) {
+      if (event.target.id == "celsius") {
+        weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed()}°C outside.`;
+        // console.log(weatherText + "1")
+        document.querySelector("p#weather").innerHTML = weatherText;
+      } else {
+        weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${celsiusToFahr(
+          temperature
+        ).toFixed()}°F outside.`;
+        // §console.log(weatherText + "2");
+        document.querySelector("p#weather").innerHTML = weatherText;
+      }
+    });
 
-
-// console.log(weatherText); (event.target.id) === "celsius"
-
-// !! Farh to Celsius => Redio Buttons !!
-
-document.querySelector(".weather-group").addEventListener('click', function(event) {
-   if  ((event.target.id) == "celsius") {
-    weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${temperature.toFixed()}°C outside.`;
-    // console.log(weatherText + "1")
-    document.querySelector("p#weather").innerHTML = weatherText;
-   } else {
-    weatherText = `The weather is ${weatherCondition} in ${userLocation} and it's ${celsiusToFahr(temperature).toFixed()}°F outside.`;
-    // §console.log(weatherText + "2");
-    document.querySelector("p#weather").innerHTML = weatherText;
-   }
-});
-
-document.querySelector("#greeting").innerHTML = greetingText;
-
+  document.querySelector("#greeting").innerHTML = greetingText;
+}
 // !! Clock !!
-
-
 
 //  new Date().getHours();
 // document.querySelector("span[data-time=hours]").textContent = localTime.getHours();
@@ -55,17 +131,26 @@ document.querySelector("#greeting").innerHTML = greetingText;
 // // // new Date().getSeconds();
 // document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds();
 // console.log('outside the timeout')
-
-setInterval(function(){
+function clockHandler() {
+  setInterval(function () {
     let localTime = new Date();
     // console.log('inside the timeout');
-    document.querySelector("span[data-time=hours]").textContent = localTime.getHours().toString().padStart(2, "0");;
-// // // new Date().getMinutes()
-    document.querySelector("span[data-time=minutes]").textContent = localTime.getMinutes().toString().padStart(2, "0");
-// // // new Date().getSeconds();
-    document.querySelector("span[data-time=seconds]").textContent = localTime.getSeconds().toString().padStart(2, "0");
-}, 1000);
-
+    document.querySelector("span[data-time=hours]").textContent = localTime
+      .getHours()
+      .toString()
+      .padStart(2, "0");
+    // // // new Date().getMinutes()
+    document.querySelector("span[data-time=minutes]").textContent = localTime
+      .getMinutes()
+      .toString()
+      .padStart(2, "0");
+    // // // new Date().getSeconds();
+    document.querySelector("span[data-time=seconds]").textContent = localTime
+      .getSeconds()
+      .toString()
+      .padStart(2, "0");
+  }, 1000);
+}
 // ?? Loop for ??
 // let animals = ['dogs', 'cat', 'lion', 'zebra'];
 // for (let i = 0; i < animals.length; i++) {
@@ -85,26 +170,150 @@ setInterval(function(){
 //         }
 //     }
 //    }
-   
+
 //    printEXAccounts();
 
 // !! Galery !!
 // src="./assets/gallery/image1.jpg" alt="Thumbnail Image 1"
 
-// const galleryImages = [
-//     {
-//         src: "./assets/gallery/image1.jpg",
-//         alt: "Thumbnail Image 1"
-//     },
-//     {
-//         src: "./assets/gallery/image2.jpg",
-//         alt: "Thumbnail Image 2"
-//     },
-//     {
-//         src: "./assets/gallery/image2.jpg",
-//         alt: "Thumbnail Image 2"
-//     }
-// ];
+/* for (let i in galleryImages) {
+    console.log(galleryImages[i]);
+} */
 
-// for (img in galleryImages) {
-//     console.log(img);
+// ?? ForEach loop ??
+
+  /* <img src="./assets/gallery/image1.jpg" alt="Thumbnail Image 1" data-array-index="0" data-selected="true">
+<img src="./assets/gallery/image2.jpg" alt="Thumbnail Image 2" data-array-index="1" data-selected="false">
+<img src="./assets/gallery/image3.jpg" alt="Thumbnail Image 3" data-array-index="2" data-selected="false"> */
+
+
+// console.log(mainImage);
+function galleryHandler() {
+  let mainImage = document.querySelector("#gallery > img");
+  let thumbnail = document.querySelector("#gallery .thumbnails");
+  // console.log(thumbnail);
+  mainImage.src = galleryImages[0].src;
+  mainImage.alt = galleryImages[0].alt;
+  galleryImages.forEach(function (image, index) {
+    let thumb = document.createElement("img");
+    thumb.src = image.src;
+    thumb.alt = image.alt;
+    thumb.dataset.arrayIndex = index;
+    // Ternary Conditional
+    thumb.dataset.selected = index === 0 ? true : false;
+    thumb.addEventListener("click", function (e) {
+      let selectedIndex = e.target.dataset.arrayIndex;
+      let selectedImage = galleryImages[selectedIndex];
+      mainImage.src = selectedImage.src;
+      mainImage.alt = selectedImage.alt;
+      thumbnail.querySelectorAll("img").forEach(function (img) {
+        img.dataset.selected = false;
+      });
+
+      e.target.dataset.selected = true;
+      console.log(selectedIndex);
+    });
+    // console.log(thumb)
+    /* if (index === 0) {
+        thumb.dataset.selected = true;
+    } else {
+        thumb.dataset.selected = false;
+    } */
+    // console.log(thumb);
+    thumbnail.appendChild(thumb);
+  });
+}
+
+// Product section
+
+/* <div class="product-item">
+<img src="./assets/products/img6.png" alt="AstroFiction">
+<div class="product-details">
+   <h3 class="product-title">AstroFiction</h3>
+   <p class="product-author">John Doe</p>
+   <p class="price-title">Price</p>
+   <p class="product-price">$ 49.90</p>
+</div>
+</div> */
+
+function productHandler() {
+
+    let productSection = document.querySelector(".products-area");
+    
+    // Run a looop through the products and create an HTML element ("product-iteem") for each of them
+    product.forEach(function (product, index) {
+         // Create the HTML elements for the individual products
+        let productElm = document.createElement("div");
+        productElm.classList.add("product-item");
+
+        // Create product images
+        let productImage = document.createElement("img");
+        productImage.src = product.image;
+        productImage.alt = "Image for " + product.title;
+        
+        // Create product details 
+        let productDetails = document.createElement("div");
+        productDetails.classList.add("product-details");
+
+        // Create product title, author, price-title and price
+        let productTitle = document.createElement("h3");
+        productTitle.classList.add('product-title');
+        productTitle.textContent = product.title;
+        
+        let productAuthor = document.createElement("p");
+        productAuthor.classList.add('product-author');
+        productAuthor.textContent = product.author;
+
+        let productPriceTitle = document.createElement("p");
+        productPriceTitle.classList.add('price-title');
+        productPriceTitle.textContent = "Price";   
+
+        let productPrice = document.createElement("p");
+        productPrice.classList.add('price');
+        productPrice.textContent = product.price == 0 ? "Free" : "$" + product.price.toFixed(2);
+
+        // Append product details 
+        productDetails.append(productTitle);
+        productDetails.append(productAuthor);
+        productDetails.append(productPriceTitle)
+        productDetails.append(productPrice)
+
+        // Add all child HTML elements of the product
+        productElm.append(productImage);
+        productElm.append(productDetails);
+        
+        // Add the complete individual product to the product section
+        productSection.append(productElm);
+    });
+
+    // products-filter
+
+    // let totalProducts = product.length;
+    document.querySelector(".products-filter label[for=all] span.product-amount").textContent = product.length;
+
+    let totalPaid = product.filter(function(item) {
+        return item.price > 0; 
+    })
+    // console.log(totalPaid.length);
+    document.querySelector(".products-filter label[for=paid] span.product-amount").textContent = totalPaid.length;
+
+    let free = product.filter(function(item) {
+        return !item.price || item.price <= 0; 
+    })
+
+    document.querySelector(".products-filter label[for=free] span.product-amount").textContent = free.length;
+}
+// let numbers = [1,3,4,5,5,6,7,4,2,6];
+
+// let greaterThan4 = numbers.filter(function(item){
+//     return item > 4;
+// })
+
+// console.log(greaterThan4);
+
+// Page Load
+manuHandler();
+greetingHandler();
+clockHandler();
+galleryHandler();
+productHandler();
